b53ff2db350fc0c9fee2e869d7e3f5111edc8750
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-10-21 11:17:32 +0900

    add generic for select props

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,15 +1,19 @@
 module.exports = {
-    stories: [
-        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        '@storybook-addon-mock/register'
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: 'webpack5',
+  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        background: false,
+      },
     },
+    '@storybook/addon-interactions',
+    '@storybook-addon-mock/register',
+    'storybook-addon-themes',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
 };
