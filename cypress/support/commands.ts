73280ef44858a010fbc6bd6b5e559c4afa771852
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-10-24 15:20:58 +0900

    add e2e test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,26 +1,13 @@
-/// <reference types="cypress" />
+import * as commonCommands from './commands/common';
+import * as profileCommands from './commands/profile';
+import * as articleCommands from './commands/article';
+import * as commentsCommands from './commands/comments';
+import * as ratingCommands from './commands/rating';
 
-// -- This is a parent command --
-Cypress.Commands.add('login', (email:string, password:string):void => {  })
+Cypress.Commands.addAll(commonCommands);
+Cypress.Commands.addAll(profileCommands);
+Cypress.Commands.addAll(articleCommands);
+Cypress.Commands.addAll(commentsCommands);
+Cypress.Commands.addAll(ratingCommands);
 
-
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>
-
-    }
-  }
-}
-export {}
+export {};
