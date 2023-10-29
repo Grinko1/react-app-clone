11d20d850eb83fe7314237adaba60ee6bb58e4f7
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-10-29 14:57:47 +0900

    fix skeleton and sidebar item color,remove feature

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -14,9 +14,10 @@ export const AppLogo = memo((props: AppLogoProps) => {
   const { className, size=50 } = props;
   return (
     <HStack max justify='center' className={classNames(cls.appLogoWrapper, {}, [className])}>
+      <AppSvg className={cls.appLogo} width={size } height={size } />
       <div className={cls.gradientBig} />
       <div className={cls.gradientSmall} />
-      <AppSvg className={cls.appLogo} width={size } height={size } />
+      
     </HStack>
   );
 });
